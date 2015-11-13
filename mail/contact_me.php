fb76c65115ea144696b59d876c164c5e1f4be3a0
jpedra <john.pedra@gmail.com>
2015-11-13 13:32:38 +0000

    Change the text written to the contact.txt

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -9,7 +9,7 @@ function writeTofile($message) {
       echo $ex->getMessage();
       return $ex->getMessage();
    }
-   return "true";
+   return true;
 }
 
 // Check for empty fields
@@ -32,10 +32,11 @@ $message = $_POST['message'];
 $to = 'john.pedra@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Findyl Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$file_body = "Name: $name | Email: $email_address | Phone: $phone | Message:$message\n";
 $headers = "From: john.pedra@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 
-return writeTofile($email_body);
+return writeTofile($file_body);
 //mail($to,$email_subject,$email_body,$headers);
 //return true;			
 ?>
